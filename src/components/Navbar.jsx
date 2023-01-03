e84c64a07dca811db375cd303b4d3bb960c512b8
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-03 00:15:57 -0500

    modificado error deploy  p por a  y css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                <NavLink className={({isActive}) => isActive ? "navelementact" : "navelement"} to="/imgcalc">IMC Calculator</NavLink>
                <NavLink className={({isActive}) => isActive ? "navelementact" : "navelement"} to="/agecalc">Age Calculator</NavLink>
                <div className="logo"><img src="../img/logo.png" alt="Logo kruger"/>
-               <p > Kruger Start</p></div> 
+               <p>Kruger Start</p></div> 
                <NavLink className={({isActive}) => isActive ? "navelementact" : "navelement"} to="/clock">My Clock</NavLink>
                <NavLink className={({isActive}) => isActive ? "navelementact" : "navelement"} to="/about">About Me</NavLink>
                <NavLink className={({isActive}) => isActive ? "navelementact" : "navelement"} to="/contact">Contact</NavLink>
